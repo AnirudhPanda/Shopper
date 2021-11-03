f4b90f358887051f365ee011f35ca2632280e5bd
anirudhpanda <anirudhpandaofficial@gmail.com>
2021-11-03 17:01:27 +0530

    header and footer component done with rb

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Row>
+        <Col className='text-center py-3'>Made with 💜 by Anirudh</Col>
+      </Row>
+    </footer>
+  )
+}
+
+export default Footer
